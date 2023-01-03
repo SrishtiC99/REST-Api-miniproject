846149f75afd799a9b98591a2749d7ac5207c217
SrishtiC99 <iamsri9631@gmail.com>
2023-01-03 18:36:39 +0530

    Used chained route handlers

--- app.js
+++ app.js
@@ -22,46 +22,44 @@ const articleSchema = new mongoose.Schema({
 
 const Article = new mongoose.model("Article", articleSchema);
 
-// GET: Fetch all the articles
-app.get("/articles", function(req, res){
-  Article.find(function(err, foundArticles){
-    if(!err){
-      res.send(foundArticles);
-    }else{
-      res.send(err);
-    }
+app.route("/articles")
+  // GET: Fetch all the articles
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    })
   })
-})
-
-// POST: Creates one new article
-app.post("/articles", function(req, res){
-  let title = req.body.title;
-  let content = req.body.content;
-  const newArticle = new Article({
-    title: title,
-    content: content
+  // POST: Creates one new article
+  .post(function(req, res) {
+    let title = req.body.title;
+    let content = req.body.content;
+    const newArticle = new Article({
+      title: title,
+      content: content
+    })
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Successfully added a new article!");
+      } else {
+        res.send(err);
+      }
+    });
   })
-  newArticle.save(function(err){
-    if(!err){
-      res.send("Successfully added a new article!");
-    }
-    else{
-      res.send(err);
-    }
+  // DELETE: Delete all the articles
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted all the articles!");
+      } else {
+        console.log(err);
+        res.send(err);
+      }
+    })
   });
-})
-
-// DELETE: Delete all the articles
-app.delete("/articles", function(req, res){
-  Article.deleteMany(function(err){
-    if(!err){
-      res.send("Successfully deleted all the articles!");
-    } else{
-      console.log(err);
-      res.send(err);
-    }
-  })
-})
 
 app.listen("3000", function(req, res){
   console.log("Server is running at port 3000");
